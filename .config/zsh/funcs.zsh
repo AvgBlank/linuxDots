20ce085c84ea376754565451ba3b5b1edf006504
Husain Khorakiwala <hussainkhorakiwala45@gmail.com>
2025-02-25 01:20:26 +0530

    Add

--- .config/zsh/funcs.zsh
+++ .config/zsh/funcs.zsh
@@ -71,10 +71,10 @@ function tmuxthing() {
             WIN_DIR="/mnt/c/Users/$WIN_HOME"
             
             # Run find in both Linux and Windows home and coding directories
-            selected=$(find ~/ ~/Projects /mnt/c/Projects "$WIN_DIR" "$WIN_DIR/Projects" -mindepth 1 -maxdepth 1 -type d | fzf)
+            selected=$(find ~/ ~/Projects /mnt/c/Projects "$WIN_DIR" "$WIN_DIR/Projects" -mindepth 1 -maxdepth 2 -type d | fzf)
         else
             # Run find in Linux home and coding directories
-            selected=$(find ~/ ~/Projects -mindepth 1 -maxdepth 1 -type d | fzf)
+            selected=$(find ~/ ~/Projects -mindepth 1 -maxdepth 2 -type d | fzf)
         fi
     fi
 
