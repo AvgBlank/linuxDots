912d418c1531af252c1b2ac5f8e390cbb0a7ae06
Hussain Khorakiwala <hussainkhorakiwala45@gmail.com>
2026-04-13 12:23:43 +0530

    feat: change up how keymaps work to use zsh widgets

--- .config/zsh/keymaps.zsh
+++ .config/zsh/keymaps.zsh
@@ -8,17 +8,33 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 # Ctrl + L to clear the screen
 bindkey -s '^L' "clear\n"
-# Ctrl + F to open tmux script
-bindkey -s '^F' 'tmuxthing\n'
-# Ctrl + T to open vim script
-bindkey -s '^T' 'vimthing\n'
-# Ctrl + Y to open yazi script
-bindkey -s '^Y' 'yazithing\n'
-# Ctrl + E to open change dir script
-bindkey -s '^E' 'cdthing\n'
 # Ctrl + U to open history search and accept query.
 zle -N fzfHistory
 bindkey '^R' fzfHistory
 # Ctrl + R to just open history search.
 zle -N fzfAcceptHistory
 bindkey '^U' fzfAcceptHistory
+
+# Ctrl + F to open tmux script
+tmuxthing_widget() {
+  BUFFER="tmuxthing"
+  zle accept-line
+}
+zle -N tmuxthing_widget
+bindkey '^F' tmuxthing_widget
+# Ctrl + T to open vim script
+vimthing_widget() {
+  vimthing
+  zle reset-prompt
+}
+zle -N vimthing_widget
+bindkey '^T' vimthing_widget
+# Ctrl + Y to open yazi script
+yazithing_widget() {
+  yazithing
+  zle reset-prompt
+}
+zle -N yazithing_widget
+bindkey '^Y' yazithing_widget
+# Ctrl + E to open change dir script
+bindkey -s '^E' 'cdthing\n'
